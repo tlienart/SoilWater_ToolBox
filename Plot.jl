88e508da113d4fae0c86fab769eead67075a4f6b
jesusfg <jesusfg@ugr.es>
2019-11-15 05:05:04 +1300

    clean up Winston plots

--- Plot.jl
+++ Plot.jl
@@ -65,25 +65,19 @@ module plot
 				Winston.setattr(Plot_θ_Ψ.y1, label="θ [cm^3 cm^{-3}]", range=(0.0, θ_θΨ_Max))
 				Obs_θ_Ψ = Winston.Points(1.0 .+ Ψ_θΨ[iSoil,1:N_θΨ[iSoil]] .* cst.mm_2_cm, θ_θΨ[iSoil,1:N_θΨ[iSoil]], color="red")
 				Sim_θ_Ψ = Winston.Curve(Ψ_Sim .* cst.mm_2_cm, θ_Sim, color="blue")
-				#θ_Ψ = Winston.semilogx(1.0 .+ Ψ_θΨ[iSoil,1:N_θΨ[iSoil]] .* cst.mm_2_cm, θ_θΨ[iSoil,1:N_θΨ[iSoil]], "g^", Ψ_Sim .* cst.mm_2_cm, θ_Sim, "b-o")
 				θ_Ψ = Winston.add(Plot_θ_Ψ, Obs_θ_Ψ, Sim_θ_Ψ) 
 				#Winston.savefig(θ_Ψ, Path)
 
 				# Plot K(Ψ)
-				Plot_K_θ = Winston.FramedPlot(aspect_ratio=1)  
-				# xlabel="Ψ [cm]",
-				# ylabel="K(Ψ) [cm \ h^{-1}]")
-				Winston.setattr(Plot_K_θ.x1, label="Ψ [cm]", range=(0.1, Ψ_θΨ_Max*cst.mm_2_cm), log=true)
-				Winston.setattr(Plot_K_θ.y1, label="K(Ψ) [cm h^{-1}]")
-				Obs_K_θ = Winston.Points(1.0 .+ Ψ_KΨ[iSoil,1:N_KΨ[iSoil]] .* cst.mm_2_cm, K_KΨ[iSoil,1:N_KΨ[iSoil]] * cst.mms_2_cmh, color="red")
-				Sim_K_θ = Winston.Curve(Ψ_Sim .* cst.mm_2_cm, Kunsat_Sim .* cst.mms_2_cmh, color="blue")
-				K_θ = Winston.add(Plot_K_θ, Obs_K_θ, Sim_K_θ)
-
-				# Winston.add(Plot_K_θ, K_θ) 
+				Plot_K_Ψ = Winston.FramedPlot(aspect_ratio=1)  
+				Winston.setattr(Plot_K_Ψ.x1, label="Ψ [cm]", range=(0.1, Ψ_θΨ_Max*cst.mm_2_cm), log=true)
+				Winston.setattr(Plot_K_Ψ.y1, label="K(Ψ) [cm h^{-1}]")
+				Obs_K_Ψ = Winston.Points(1.0 .+ Ψ_KΨ[iSoil,1:N_KΨ[iSoil]] .* cst.mm_2_cm, K_KΨ[iSoil,1:N_KΨ[iSoil]] * cst.mms_2_cmh, color="red")
+				Sim_K_Ψ = Winston.Curve(Ψ_Sim .* cst.mm_2_cm, Kunsat_Sim .* cst.mms_2_cmh, color="blue")
+				K_θ = Winston.add(Plot_K_Ψ, Obs_K_Ψ, Sim_K_Ψ)
 
 				MultiPlots[1,1] = Plot_θ_Ψ
-				MultiPlots[1,2] = Plot_K_θ
-
+				MultiPlots[1,2] = Plot_K_Ψ
 				Winston.savefig(MultiPlots, Path)
 
 
