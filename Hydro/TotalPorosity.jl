7094e447ee13f62cae00473dd333afe43cefe92b
joseph-pollacco <pollaccoj@landcareresearch.co.nz>
2019-12-19 22:40:37 +1300

    Total Porosity 2

--- Hydro/TotalPorosity.jl
+++ Hydro/TotalPorosity.jl
@@ -13,7 +13,13 @@ module Φ
 
 			for iSoil=1:N_SoilSelect
 				# Φ_Fine = 1.0 - ρbSoil[iSoil] / ρp_Fine[iSoil]
-				Φ_Fine = 1.0 - (RockW[iSoil]/ρ_Rock[iSoil]) - (1-RockW[iSoil])/((1-RockW[iSoil]/ρ_Rock)*ρp_Fine[iSoil])
+				# [Volume Rock in soil] - [Fine earth bulk density]
+				Φ_Fine = 1.0 - (RockW[iSoil] / ρ_Rock[iSoil]) - (1.0 - RockW[iSoil]) / ((1.0 - RockW[iSoil] / ρ_Rock[iSoil]) * ρp_Fine[iSoil])
+
+
+				# Vrock = RockW[iSoil] / ρ_Rock[iSoil]
+
+
 				Φ[iSoil] = Φ_Fine
 			end # for
 			
