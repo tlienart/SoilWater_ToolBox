19fb42d74c63558075857dafc399249d6c093a74
jesusfg <jesusfg@ugr.es>
2019-12-20 00:44:29 +1300

    function FLUXCONS_Se

--- Infilt/Sorptivity2.jl
+++ Infilt/Sorptivity2.jl
@@ -4,6 +4,7 @@
 module sorptivity
 	import ..wrc, ..kunsat, ..option
 	import QuadGK
+	import SpecialFunctions: erfc, erfcinv
 	export SORPTIVITY, DIFFUSIVITY
 
 
@@ -28,9 +29,21 @@ module sorptivity
 	end 
 
 	
-	function FLUXCONS_Se(Se, Se_Ini)
-		FluxConcent = 2.0 * (Se - Se_Ini) / (1.0 + Se - 2.0 * Se_Ini)
-	end
+	function FLUXCONS_Se(Se, Se_Ini, Se_Sat)
+	
+		if option.infilt.SorptivityModel == "Parlange"
+			return FluxConcent = 2.0 * (Se - Se_Ini) / (Se_Sat + Se - 2.0 * Se_Ini)
+		elseif  option.infilt.SorptivityModel == "Crank"
+			return FluxConcent = exp(-erfcinv((Se - Se_Ini/(Se_Sat-Se_Ini))))
+		elseif option.infilt.SorptivityModel == "Philip&Knight"
+			return FluxConcent = (Se - Se_Ini) / (Se_Sat - Se_Ini)
+		elseif option.infilt.SorptivityModel == "Brutsaert"
+			return FluxConcent = ((Se - Se_Ini) / (Se_Sat - Se_Ini)^0.5)
+		end # option.infilt
+
+	end # FLUXCONS_Se
+
+
 
 	function DIFFUSIVITY(θ, iSoil, hydro; Diffusivity_Min=10^-5.0) #-5		
 		Kunsat = kunsat.θ_2_KUNSAT(θ, iSoil, hydro)
